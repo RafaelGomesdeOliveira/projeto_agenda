e8bec965e5cc4d8458267885f8669a6cd872dc43
Rafael Gomes <rafaelgomesdeoliveiraa@gmail.com>
2024-03-18 23:16:40 -0300

    Teste

--- project/settings.py
+++ project/settings.py
@@ -87,6 +87,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
+
+#Validadores de senha
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
