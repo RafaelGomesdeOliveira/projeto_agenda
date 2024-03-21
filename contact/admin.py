3c70cddf2374370f275de058a759fa756d08eb15
Rafael Gomes <rafaelgomesdeoliveiraa@gmail.com>
2024-03-21 10:13:53 -0300

    Adicionei a foreign key category no model de contact

--- contact/admin.py
+++ contact/admin.py
@@ -5,7 +5,7 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContantAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'phone', 'description'] #Campos que vão aparecer
+    list_display = ['id', 'first_name', 'last_name', 'phone', 'description', 'show', 'picture'] #Campos que vão aparecer
     ordering = ['-id'] #Ordem decrecente de exibição
     list_filter = ['created_date'] #Filtro que vai ficar ao lado, criados na ultima semana, no ultimo mês, etc
     search_fields = ['id', 'first_name', 'last_name'] # Campos que eu posso buscar
@@ -13,3 +13,7 @@ class ContantAdmin(admin.ModelAdmin):
     list_max_show_all = 1000 #Só vai mostrar todos se tiver no máximo 1000 usuário
     list_editable = ['first_name', 'last_name'] # Campos que podem ser editados mais facilmente
     list_display_links = ['id', 'phone'] # Link para acessar a página de edição
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
--- contact/models.py
+++ contact/models.py
@@ -4,6 +4,11 @@ from django.utils import timezone
 # Create your models here.
 #ID é automático
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return f'{self.name}'
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -11,6 +16,10 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True) #Não precisa informar informar a limitação, mas pode informar
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/') #Se obrigar a ter imagem e conter contatos sem, vai acabar gerando um erro
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True) #Uma categoria para vários contatos linkados 
+    #CASCADE apaga os contatos quando eu apagar a categoria // SET_NULL seta como nulo quando apago a categoria, mas eu tenho que informar o blank=True e Null = True
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
