cbd9bdec25c91175aa0374761f205e24e026b76c
Rafael Gomes <rafaelgomesdeoliveiraa@gmail.com>
2024-03-22 07:17:26 -0300

    Adicionei as configurações para ter acesso ao local_settings.py

--- project/settings.py
+++ project/settings.py
@@ -135,3 +135,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+#Isso é importante para puxar as configurações do local_settings
+try: 
+    from project.local_settings import *
+except ImportError:
+    ...
